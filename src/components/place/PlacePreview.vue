6c7f4bac77be2ed9deeb2e1f2dfd7010f247a5b1
kerendot <kerendot@gmail.com>
2017-06-25 09:35:51 +0300

    moved home to seperate cmp

--- src/components/place/PlacePreview.vue
+++ src/components/place/PlacePreview.vue
@@ -1,32 +1,26 @@
 <template>
     <section class="place-preview">
         <li>
-            <ui-collapsible @open="selectPlace" :title="place.name">
+            <ui-collapsible id="test" @open="selectPlace" :title="place.name">
                 <h4>{{place.address}}</h4>
                 <place-details :place="place">
                 </place-details>
-        
+    
                 <ui-icon-button @click.stop="deletePlace" icon="delete" tooltip="Delete" tooltip-position="top center"></ui-icon-button>
                 <ui-icon-button @click.stop="editPlace" icon="edit" tooltip="Edit" tooltip-position="top center"></ui-icon-button>
-           
+    
                 <ui-modal ref="editModal">
                     <div slot="header">
                         <h1>Edit Saved Place: {{place.name}}</h1>
                     </div>
-                      
-                    <ui-textbox floating-label label="Name" 
-                    placeholder="Enter your name" 
-                    v-if="editMode"
-                    v-model="placeToEdit.name"></ui-textbox>
-                    
-                     <ui-textbox floating-label label="Tags (comma seperated)" 
-                    placeholder="Enter comma seperated tags" 
-                    v-if="editMode"
-                    v-model="placeToEdit.tags"></ui-textbox>
-
+    
+                    <ui-textbox floating-label label="Name" placeholder="Enter your name" v-if="editMode" v-model="placeToEdit.name"></ui-textbox>
+    
+                    <ui-textbox floating-label label="Tags (comma seperated)" placeholder="Enter comma seperated tags" v-if="editMode" v-model="placeToEdit.tags"></ui-textbox>
+    
                     <ui-button @click="save" color="primary" icon="done" :icon-position="iconPosition" type="secondary">Apply</ui-button>
                     <!--<ui-button @click="cancel" color="orange" icon="delete" :icon-position="iconPosition" type="secondary">Dismiss</ui-button>-->
-                            
+    
                 </ui-modal>
     
             </ui-collapsible>
@@ -47,8 +41,8 @@ export default {
     data() {
         return {
             editMode: false,
-            placeToEdit:null,
-            iconPosition:'Left'
+            placeToEdit: null,
+            iconPosition: 'Left'
         }
     },
     methods: {
@@ -61,7 +55,7 @@ export default {
         },
         editPlace() {
             this.placeToEdit = JSON.parse(JSON.stringify(this.place));
-            this.placeToEdit.tags =  this.placeToEdit.tags.join(', ');
+            this.placeToEdit.tags = this.placeToEdit.tags.join(', ');
             this.editMode = true;
             this.openModal('editModal');
         },
